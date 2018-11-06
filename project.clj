e49f2f5d4927803a65729dc74c1db5af842bc677
Matthew Marks <matthewfmarks@gmail.com>
2018-11-06 16:39:34 -0500

    first

---
+++ project.clj
@@ -0,0 +1,19 @@
+(defproject clj-nginx "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [compojure "1.6.1"]
+                 [ring/ring-defaults "0.3.2"]]
+  :plugins [[lein-ring "0.12.4"]]
+  :ring {:handler clj-nginx.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring/ring-mock "0.3.2"]]}}
+
+  :jvm-opts ["-Xmx3g"
+             "-XX:+StartAttachListener"
+             "-Dcom.sun.management.jmxremote"
+             "-Dcom.sun.management.jmxremote.ssl=false"
+             "-Dcom.sun.management.jmxremote.authenticate=false"
+             "-Dcom.sun.management.jmxremote.port=43210"])
